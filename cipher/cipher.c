167d3d0f6286a577f951582f96025ef39425524b
Kelvin <donebydunn@gmail.com>
2021-07-29 21:35:12 -0600

    new alg & style

--- cipher/cipher.c
+++ cipher/cipher.c
@@ -2,67 +2,99 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <search.h>
 
 #define ALPHABET_LEN 26
 #define ALPHABET "abcdefghijklmnopqrstuvwxyz"
 #define MAX_INPUT_LEN 1000
 #define SCRAMBLE_LEN 24
 
-int has_dupe(int val, int dupes[], int *last_dupe);
+
+void process_input(char input[]); 
+int chrcmp(const void *a, const void *b); 
+void print_scramble(char uniques[], size_t last_unique);
+int find_scramble_index(char letter, char *sorted_uniques, int stop); 
+void print_unused(char *unused); 
 
 int main(int argc, char *argv[]) {
-	int cipher[ALPHABET_LEN];
-	int i, j, k, input_len, last_dupe, last_scramble, alpha_num;
-	char alpha[ALPHABET_LEN] = ALPHABET;
-	char input[MAX_INPUT_LEN]; 
-	int o_scramble[SCRAMBLE_LEN] = {0};
-	int dupes[SCRAMBLE_LEN];
-	char lower;
-
-	for (i = 0; i < ALPHABET_LEN; i++) {
-		cipher[i] = i + 1;
-	}
-	last_dupe = 0;
-	last_scramble = 0;
 	if (argc != 2) {
 		fprintf(stderr,"%s expects a single string argument.\n", argv[0]);
 		exit(1);
 	} 
 	else {
-		strcpy(input, argv[1]);
-		input_len  = strlen(input);
-		for (i = 0; i < input_len; i++) {
-			if (isalpha(input[i])) {
-				lower = tolower(input[i]);
-				for (j = 0; j < ALPHABET_LEN; j++) {
-					if (alpha[j] == lower) {
-						alpha_num = j + 1;
-						if (j < SCRAMBLE_LEN && !has_dupe(alpha_num, dupes, &last_dupe)) {
-							o_scramble[last_scramble++] = alpha_num;
-						}
-					}
-				}
-			} 
-		}
-	}
-	for (i = 0; i < SCRAMBLE_LEN; i++) {
-		if (!o_scramble[i]) {
-			printf("%-2d X\n", i + 1);
-		}
-		else {
-			printf("%-2d %-2d %c\n", i + 1, o_scramble[i], alpha[o_scramble[i] - 1]);
-		}
+		process_input(argv[1]);
 	}
 	return 0;
 }
 
-int has_dupe(int val, int dupes[], int *last_dupe) {
+void process_input(char input[]) {
+	int input_len = strlen(input);
 	int i;
-	for (i = 0; i < *last_dupe; i++) {
-		if (dupes[i] == val) {
-			return 1;
+	size_t last_unique = 0;
+	char uniques[SCRAMBLE_LEN] = {0};
+	char lower;
+
+	for (i = 0; i < input_len; i++) {
+		if (isalpha(input[i])
+		&& (lower = tolower(input[i]))
+		&& !lfind(&lower, uniques, &last_unique, sizeof(char), chrcmp)
+		&& last_unique < SCRAMBLE_LEN) {
+			uniques[last_unique++] = lower;
 		}
 	}
-	dupes[(*last_dupe)++] = val;
-	return 0;
+	print_scramble(uniques, last_unique);
+}
+
+int chrcmp(const void *a, const void *b) {
+
+	return *(const char *)a - *(const char *)b;
+}
+
+void print_scramble(char uniques[], size_t last_unique) {
+	char sorted_uniques[last_unique];
+	int scramble[SCRAMBLE_LEN] = {0};
+	char not_used[SCRAMBLE_LEN];
+	int last_not_used = 0;
+	char alphabet[] = ALPHABET;
+	int alphabet_len = ALPHABET_LEN;
+	int i;
+
+	for (i = 0; i < alphabet_len; i++) {
+		if (!lfind(&alphabet[i], uniques, &last_unique, sizeof(char), chrcmp)) {
+			not_used[last_not_used++] = alphabet[i];
+		}
+	}
+	strcpy(sorted_uniques, uniques);
+	qsort(sorted_uniques, last_unique, sizeof(char), chrcmp);
+	for (i = 0; i < last_unique; i++){
+		scramble[i] = find_scramble_index(uniques[i], sorted_uniques, last_unique);
+	}
+	for (i = 0; i < SCRAMBLE_LEN; i++) {
+		if (!scramble[i]) {
+			printf("%-2d X  ", i + 1);
+			print_unused(not_used);
+			printf("\n");
+		}
+		else {
+			printf("%-2d %-2d %c\n", i + 1, scramble[i], uniques[i]);
+		}
+	}
+}
+
+int find_scramble_index(char letter, char *sorted_uniques, int stop) {
+	int index = 1;
+
+	while (letter != *sorted_uniques++ && index < stop) 
+		index++;
+	return index;
+}
+
+void print_unused(char *unused) {
+	char letter;
+
+	if (letter = *unused++) {
+		printf("%c", letter);
+		while (letter = *unused++) 
+			printf(", %c", letter);
+	}
 }
